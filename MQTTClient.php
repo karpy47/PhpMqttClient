506e05bfa585aff4242c791f5f3b75bab9e319a2
karpy47 <karpy47@users.noreply.github.com>
2018-11-22 21:17:03 +0100

    Update MQTTClient.php
    Fixed #7 and #6

--- MQTTClient.php
+++ MQTTClient.php
@@ -3,7 +3,7 @@
  * Implementation of MQTT 3.1.1 for PHP with SSL support
  */
 
-namespace karpy47/PhpMqttClient;
+namespace karpy47\PhpMqttClient;
 
 class MQTTClient {
 
@@ -426,7 +426,7 @@ class MQTTClient {
 	        }
 
 	        // Send PUBREL
-	        $this->sendPubRel($receivedPacketId);
+	        $this->sendPubRel($response['packetId']);
 
 	        // A PUBCOMP packet is expected
 	        $response = $this->waitForPacket(self::MQTT_PUBCOMP, $packetId);
